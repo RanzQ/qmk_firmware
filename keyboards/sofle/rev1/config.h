15d9630cc19aae6784abdfceb1301e6257895669
RanzQ <juha@codercoded.com>
2023-08-05 18:45:18 +0300

    rebase from origin/master and fix conflicts

--- keyboards/sofle/rev1/config.h
+++ keyboards/sofle/rev1/config.h
@@ -3,12 +3,4 @@
 #define TAPPING_TERM 100
 #define DEBOUNCE 5
 
-/* encoder support */
-#define ENCODERS_PAD_A { F5 }
-#define ENCODERS_PAD_B { F4 }
-#define ENCODERS_PAD_A_RIGHT { F4 }
-#define ENCODERS_PAD_B_RIGHT { F5 }
-#define ENCODER_RESOLUTIONS       { 4 }
-#define ENCODER_RESOLUTIONS_RIGHT { 4 }
-
 #define TAP_CODE_DELAY 10
